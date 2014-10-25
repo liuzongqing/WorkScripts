854ba0acb6991708134ee4fc2f093aff4aea74ad
zongqing.liu <zongqing.liu@funplus.com>
2014-10-25 01:30:08 -0400

    add gen.php script

---
+++ nagios/gen.php
@@ -0,0 +1,19 @@
+<?php
+
+$CONF = array(
+	'farm'	=>	array(
+		'de'	=>	array(
+			'mysql'	=>	array('check_load','check_mysql_conn','check_mysql_slow','check_disk'),
+			'proxy'	=>	array('check_load','check_haproxy','check_disk'),
+			'mongo'	=>	array('check_load','check_mongostat','check_disk'),
+			'cache'	=>	array(),
+			),
+		'fr'	=>	array(
+
+			),
+		),
+
+	);
+
+
+?>
